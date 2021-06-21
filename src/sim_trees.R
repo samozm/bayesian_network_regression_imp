5ac8c90330f0356cd5b216143b77f2411d92a73d
Sam Ozminkowski <samoz@umich.edu>
2021-06-21 14:25:04 -0500

    update for new simulation strategy (1 tree no empty cells)

--- src/sim_trees.R
+++ src/sim_trees.R
@@ -1,13 +1,10 @@
-sim_tree_dists <- function(k,chosen) {
+sim_tree_dists <- function(k) {
   if(!require("ape",character.only = TRUE, quietly = TRUE)) install.packages("ape")
-  tree <- rtree(k,tip.label=chosen)
+  tree <- rtree(k,tip.label=1:k)
   inv_dist <- 1/cophenetic.phylo(tree)
   diag(inv_dist) <- 0
   
-  ret_mat <- matrix(c(rep(0,400)),nrow=20)
-  colnames(ret_mat) <- 1:20; rownames(ret_mat) <- 1:20
   
   inv_dist <- inv_dist[order(as.integer(rownames(inv_dist))),order(as.integer(colnames(inv_dist)))]
-  ret_mat[chosen,chosen] <- inv_dist
-  return(ret_mat)
+  return(inv_dist)
 }
