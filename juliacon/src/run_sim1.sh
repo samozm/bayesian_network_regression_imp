e8ff91ba0040ac97fb083c8a7ca55ede9d21e2c6
Sam Ozminkowski <samoz@umich.edu>
2021-06-22 12:29:44 -0500

    switch to 30 total microbes 8/15 selected so it'll run on my computer

--- juliacon/src/run_sim1.sh
+++ juliacon/src/run_sim1.sh
@@ -1,26 +1,26 @@
 
 echo "unrealistic simulations for juliacon (sim 1)"
 
-echo "case 1: mean = 0.8, pi = 0.1, 10 microbes per sample"
+echo "case 1: mean = 0.8, pi = 0.1, 8 microbes per sample"
 julia simulation/run_simulation.jl -n 1 -c 1 -j
 
-echo "case 2: mean = 0.8, pi = 0.8, 10 microbes per sample"
+echo "case 2: mean = 0.8, pi = 0.8, 8 microbes per sample"
 julia simulation/run_simulation.jl -n 1 -c 2 -j
 
-echo "case 3: mean = 1.6, pi = 0.1, 10 microbes per sample"
+echo "case 3: mean = 1.6, pi = 0.1, 8 microbes per sample"
 julia simulation/run_simulation.jl -n 1 -c 3 -j
 
-echo "case 4: mean = 1.6, pi = 0.8, 10 microbes per sample"
+echo "case 4: mean = 1.6, pi = 0.8, 8 microbes per sample"
 julia simulation/run_simulation.jl -n 1 -c 4 -j
 
-echo "case 5: mean = 0.8, pi = 0.1, 10 microbes per sample"
+echo "case 5: mean = 0.8, pi = 0.1, 15 microbes per sample"
 julia simulation/run_simulation.jl -n 1 -c 5 -j
 
-echo "case 6: mean = 0.8, pi = 0.8, 10 microbes per sample"
+echo "case 6: mean = 0.8, pi = 0.8, 15 microbes per sample"
 julia simulation/run_simulation.jl -n 1 -c 6 -j
 
-echo "case 7: mean = 1.6, pi = 0.1, 10 microbes per sample"
+echo "case 7: mean = 1.6, pi = 0.1, 15 microbes per sample"
 julia simulation/run_simulation.jl -n 1 -c 7 -j
 
-echo "case 8: mean = 1.6, pi = 0.8, 10 microbes per sample"
+echo "case 8: mean = 1.6, pi = 0.8, 15 microbes per sample"
 julia simulation/run_simulation.jl -n 1 -c 8 -j
