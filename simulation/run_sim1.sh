f18705a15b08da4735983cfd1e1f5fd3306ca545
Sam Ozminkowski <samoz@umich.edu>
2021-07-13 22:36:30 -0500

    add seed to run script

--- simulation/run_sim1.sh
+++ simulation/run_sim1.sh
@@ -2,39 +2,39 @@
 echo "unrealistic simulations (sim 1)"
 
 echo "case 1: mean = 0.8, pi = 0.1, r=5, 20 microbes per sample"
-julia simulation/run_simulation.jl -n 1 -m 0.8 -p 0.1 -r 5 -k 20
+julia simulation/run_simulation.jl -n 1 -m 0.8 -p 0.1 -r 5 -k 20 -s 734 
 
 echo "case 2: mean = 0.8, pi = 0.3, r=5, 20 microbes per sample"
-julia simulation/run_simulation.jl -n 1 -m 0.8 -p 0.3 -r 5 -k 20
+julia simulation/run_simulation.jl -n 1 -m 0.8 -p 0.3 -r 5 -k 20 -s 734 
 
 echo "case 3: mean = 0.8, pi = 0.8, r=5, 20 microbes per sample"
-julia simulation/run_simulation.jl -n 1 -m 0.8 -p 0.8 -r 5 -k 20
+julia simulation/run_simulation.jl -n 1 -m 0.8 -p 0.8 -r 5 -k 20 -s 734 
 
 echo "case 4: mean = 1.6, pi = 0.1, r=5, 20 microbes per sample"
-julia simulation/run_simulation.jl -n 1 -m 1.6 -p 0.1 -r 5 -k 20
+julia simulation/run_simulation.jl -n 1 -m 1.6 -p 0.1 -r 5 -k 20 -s 734 
 
 echo "case 5: mean = 1.6, pi = 0.3, r=5, 20 microbes per sample"
-julia simulation/run_simulation.jl -n 1 -m 1.6 -p 0.3 -r 5 -k 20
+julia simulation/run_simulation.jl -n 1 -m 1.6 -p 0.3 -r 5 -k 20 -s 734 
 
 echo "case 6: mean = 1.6, pi = 0.8, r=5, 20 microbes per sample"
-julia simulation/run_simulation.jl -n 1 -m 1.6 -p 0.8 -r 5 -k 20
+julia simulation/run_simulation.jl -n 1 -m 1.6 -p 0.8 -r 5 -k 20 -s 734 
 
 echo "case 7: mean = 0.8, pi = 0.1, r=5, 50 microbes per sample"
-julia simulation/run_simulation.jl -n 1 -m 0.8 -p 0.1 -r 5 -k 50
+julia simulation/run_simulation.jl -n 1 -m 0.8 -p 0.1 -r 5 -k 50 -s 734 
 
 echo "case 8: mean = 0.8, pi = 0.3, r=5, 50 microbes per sample"
-julia simulation/run_simulation.jl -n 1 -m 0.8 -p 0.3 -r 5 -k 50
+julia simulation/run_simulation.jl -n 1 -m 0.8 -p 0.3 -r 5 -k 50 -s 734 
 
 echo "case 9: mean = 0.8, pi = 0.8, r=5, 50 microbes per sample"
-julia simulation/run_simulation.jl -n 1 -m 0.8 -p 0.8 -r 5 -k 50
+julia simulation/run_simulation.jl -n 1 -m 0.8 -p 0.8 -r 5 -k 50 -s 734 
 
 echo "case 10: mean = 1.6, pi = 0.1, r=5, 50 microbes per sample"
-julia simulation/run_simulation.jl -n 1 -m 1.6 -p 0.1 -r 5 -k 50
+julia simulation/run_simulation.jl -n 1 -m 1.6 -p 0.1 -r 5 -k 50 -s 734 
 
 echo "case 11: mean = 1.6, pi = 0.3, r=5, 50 microbes per sample"
-julia simulation/run_simulation.jl -n 1 -m 1.6 -p 0.3 -r 5 -k 50
+julia simulation/run_simulation.jl -n 1 -m 1.6 -p 0.3 -r 5 -k 50 -s 734 
 
 echo "case 12: mean = 1.6, pi = 0.8, r=5, 50 microbes per sample"
-julia simulation/run_simulation.jl -n 1  -m 1.6 -p 0.8 -r 5 -k 50
+julia simulation/run_simulation.jl -n 1 -m 1.6 -p 0.8 -r 5 -k 50 -s 734 
 
 
