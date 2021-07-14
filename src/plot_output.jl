11914ebba21f6d1aa83b987babe3a3009adb5d26
Sam Ozminkowski <samoz@umich.edu>
2021-07-13 22:38:11 -0500

    make compatible with updates to BayesianNetworkRegression

--- src/plot_output.jl
+++ src/plot_output.jl
@@ -1,18 +1,18 @@
-using Plots
+using Plots,Statistics
 include("utils.jl")
 
 
 function plot_γs_test(γ₁, γ₂, γ₁_label, γ₂_label,save_append)
-    len = size(γ₁,2)
+    len = size(γ₁,1)
     lower_bound = convert(Int,floor(len * 0.05))
     upper_bound = convert(Int,ceil(len * 0.95))
-    γ₁_sorted = sort(γ₁[1,:])
+    γ₁_sorted = sort(γ₁[:,1])
     γ₂_sorted = sort(γ₂[:,1])
     intervals = scatter([γ₁_sorted[lower_bound],median(γ₁_sorted),γ₁_sorted[upper_bound]],[1,1,1],mark=(:vline),color="red",legend=false, size=(1200,1600))
     plot!(intervals,[γ₁_sorted[lower_bound],γ₁_sorted[upper_bound]],[1,1],color="red",legend=false)
     plot!(intervals,[γ₂_sorted[lower_bound],γ₂_sorted[upper_bound]],[1,1],color="blue",legend=false)
-    for i in 2:size(γ₁,1)
-        γ₁_sorted = sort(γ₁[i,:])
+    for i in 2:size(γ₁,2)
+        γ₁_sorted = sort(γ₁[:,i])
         γ₂_sorted = sort(γ₂[:,i])
         scatter!(intervals,[γ₁_sorted[lower_bound],median(γ₁_sorted),γ₁_sorted[upper_bound]],[i,i,i],mark=(:vline),color="red",legend=false)
         plot!(intervals,[γ₁_sorted[lower_bound],γ₁_sorted[upper_bound]],[i,i],color="red",legend=false)
@@ -24,17 +24,17 @@ end
 
 
 function plot_γ_sim(γ,title,save_append,jcon)
-    len = size(γ,2)
+    len = size(γ,1)
     lower_bound = convert(Int,floor(len * 0.05))
     if lower_bound == 0
         lower_bound = 1
     end
     upper_bound = convert(Int,ceil(len * 0.95))
-    γ_sorted = sort(γ[1,:])
+    γ_sorted = sort(γ[:,1])
     intervals = scatter([γ_sorted[lower_bound],median(γ_sorted),γ_sorted[upper_bound]],[1,1,1],mark=(:vline),color="red",legend=false, size=(1200,1600))
     plot!(intervals,[γ_sorted[lower_bound],γ_sorted[upper_bound]],[1,1],color="red",legend=false)
-    for i in 2:190
-        γ_sorted = sort(γ[i,:])
+    for i in 2:size(γ,2)
+        γ_sorted = sort(γ[:,i])
         scatter!(intervals,[γ_sorted[lower_bound],median(γ_sorted),γ_sorted[upper_bound]],[i,i,i],mark=(:vline),color="red",legend=false)
         plot!(intervals,[γ_sorted[lower_bound],γ_sorted[upper_bound]],[i,i],color="red",legend=false)
     end
