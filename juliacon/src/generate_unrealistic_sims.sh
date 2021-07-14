9a3d98178e80250fb22435041ba47f30d0e3b2c3
Sam Ozminkowski <samoz@umich.edu>
2021-07-13 22:33:26 -0500

    update seed, add it to run sim file

--- juliacon/src/generate_unrealistic_sims.sh
+++ juliacon/src/generate_unrealistic_sims.sh
@@ -2,25 +2,25 @@
 echo "juliacon unrealistic simulations (sim 1) - 30 nodes total"
 
 echo "case 1: mean = 0.8, pi = 0.1, samptaxa=8"
-julia simulation/unrealistic_sim.jl -s 3323 -k 8 -t 30 -j
+julia simulation/unrealistic_sim.jl -s 734 -k 8 -t 30 -j
 
 echo "case 2: mean = 0.8, pi = 0.8, samptaxa=8"
-julia simulation/unrealistic_sim.jl -s 3323 -p 0.8 -k 8 -t 30 -j
+julia simulation/unrealistic_sim.jl -s 734 -p 0.8 -k 8 -t 30 -j
 
 echo "case 3: mean = 1.6, pi = 0.1, samptaxa=8"
-julia simulation/unrealistic_sim.jl -s 3323 -m 1.6 -k 8 -t 30 -j
+julia simulation/unrealistic_sim.jl -s 734 -m 1.6 -k 8 -t 30 -j
 
 echo "case 4: mean = 1.6, pi = 0.8, samptaxa=8"
-julia simulation/unrealistic_sim.jl -s 3323 -m 1.6 -p 0.8 -k 8 -t 30 -j
+julia simulation/unrealistic_sim.jl -s 734 -m 1.6 -p 0.8 -k 8 -t 30 -j
 
 echo "case 5: mean = 0.8, pi = 0.1, samptaxa=15"
-julia simulation/unrealistic_sim.jl -s 3323 -k 15 -t 30 -j
+julia simulation/unrealistic_sim.jl -s 734 -k 15 -t 30 -j
 
 echo "case 6: mean = 0.8, pi = 0.8, samptaxa=15"
-julia simulation/unrealistic_sim.jl -s 3323 -p 0.8 -k 15 -t 30 -j
+julia simulation/unrealistic_sim.jl -s 734 -p 0.8 -k 15 -t 30 -j
 
 echo "case 7: mean = 1.6, pi = 0.1, samptaxa=15"
-julia simulation/unrealistic_sim.jl -s 3323 -m 1.6 -k 15 -t 30 -j
+julia simulation/unrealistic_sim.jl -s 734 -m 1.6 -k 15 -t 30 -j
 
 echo "case 8: mean = 1.6, pi = 0.8, samptaxa=15"
-julia simulation/unrealistic_sim.jl -s 3323 -m 1.6 -p 0.8 -k 15 -t 30 -j
+julia simulation/unrealistic_sim.jl -s 734 -m 1.6 -p 0.8 -k 15 -t 30 -j
--- juliacon/src/run_sim1.sh
+++ juliacon/src/run_sim1.sh
@@ -2,25 +2,25 @@
 echo "unrealistic simulations for juliacon (sim 1)"
 
 echo "case 1: mean = 0.8, pi = 0.1, 8 microbes per sample"
-julia simulation/run_simulation.jl -n 1 -m 0.8 -p 0.1 -r 5 -k 8 -j
+julia simulation/run_simulation.jl -n 1 -m 0.8 -p 0.1 -r 5 -k 8 -j -s 734 
 
 echo "case 2: mean = 0.8, pi = 0.8, 8 microbes per sample"
-julia simulation/run_simulation.jl -n 1 -m 0.8 -p 0.8 -r 5 -k 8 -j
+julia simulation/run_simulation.jl -n 1 -m 0.8 -p 0.8 -r 5 -k 8 -j -s 734 
 
 echo "case 3: mean = 1.6, pi = 0.1, 8 microbes per sample"
-julia simulation/run_simulation.jl -n 1 -m 1.6 -p 0.1 -r 5 -k 8 -j
+julia simulation/run_simulation.jl -n 1 -m 1.6 -p 0.1 -r 5 -k 8 -j -s 734 
 
 echo "case 4: mean = 1.6, pi = 0.8, 8 microbes per sample"
-julia simulation/run_simulation.jl -n 1 -m 1.6 -p 0.8 -r 5 -k 8 -j
+julia simulation/run_simulation.jl -n 1 -m 1.6 -p 0.8 -r 5 -k 8 -j -s 734 
 
 echo "case 5: mean = 0.8, pi = 0.1, 15 microbes per sample"
-julia simulation/run_simulation.jl -n 1  -m 0.8 -p 0.1 -r 5 -k 15 -j
+julia simulation/run_simulation.jl -n 1  -m 0.8 -p 0.1 -r 5 -k 15 -j -s 734 
 
 echo "case 6: mean = 0.8, pi = 0.8, 15 microbes per sample"
-julia simulation/run_simulation.jl -n 1  -m 0.8 -p 0.8 -r 5 -k 15 -j
+julia simulation/run_simulation.jl -n 1  -m 0.8 -p 0.8 -r 5 -k 15 -j -s 734 
 
 echo "case 7: mean = 1.6, pi = 0.1, 15 microbes per sample"
-julia simulation/run_simulation.jl -n 1  -m 1.6 -p 0.1 -r 5 -k 15 -j
+julia simulation/run_simulation.jl -n 1  -m 1.6 -p 0.1 -r 5 -k 15 -j -s 734 
 
 echo "case 8: mean = 1.6, pi = 0.8, 15 microbes per sample"
-julia simulation/run_simulation.jl -n 1  -m 1.6 -p 0.8 -r 5 -k 15 -j
+julia simulation/run_simulation.jl -n 1  -m 1.6 -p 0.8 -r 5 -k 15 -j -s 734 
