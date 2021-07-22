f46fad09d731febd754dcfef6d80874e0f449b7e
Sam Ozminkowski <samoz@umich.edu>
2021-07-22 10:19:00 -0500

    add sleep statements to runs

--- simulation/run_sim1.sh
+++ simulation/run_sim1.sh
@@ -3,6 +3,10 @@ echo "unrealistic simulations (sim 1)"
 
 bash simulation/run_unrealistic.sh 5 10
 
+sleep 4s
+
 bash simulation/run_unrealistic.sh 10 15
+
+sleep 4s
 
 bash simulation/run_unrealistic.sh 15 20
--- simulation/run_unrealistic.sh
+++ simulation/run_unrealistic.sh
@@ -8,6 +8,8 @@ echo "mean = 0.8, pi = 0.3, r=$1, nu=$2, 22 microbes per sample"
 julia simulation/run_simulation.jl -n 1 -m 0.8 -p 0.3 -r $1 -k 22 -s 2358 -u $2
 
 
+sleep 4s
+
 echo "mean = 0.8, pi = 0.8, r=$1, nu=$2, 8 microbes per sample"
 julia simulation/run_simulation.jl -n 1 -m 0.8 -p 0.8 -r $1 -k 8 -s 2358 -u $2
 
@@ -17,6 +19,7 @@ julia simulation/run_simulation.jl -n 1 -m 0.8 -p 0.8 -r $1 -k 15 -s 2358 -u $2
 echo "mean = 0.8, pi = 0.8, r=$1, nu=$2, 22 microbes per sample"
 julia simulation/run_simulation.jl -n 1 -m 0.8 -p 0.8 -r $1 -k 22 -s 2358 -u $2
 
+sleep 4s
 
 echo "mean = 1.6, pi = 0.3, r=$1, nu=$2, 8 microbes per sample"
 julia simulation/run_simulation.jl -n 1 -m 1.6 -p 0.3 -r $1 -k 8 -s 2358 -u $2
@@ -27,6 +30,7 @@ julia simulation/run_simulation.jl -n 1 -m 1.6 -p 0.3 -r $1 -k 15 -s 2358 -u $2
 echo "mean = 1.6, pi = 0.3, r=$1, nu=$2, 22 microbes per sample"
 julia simulation/run_simulation.jl -n 1 -m 1.6 -p 0.3 -r $1 -k 22 -s 2358 -u $2
 
+sleep 4s
 
 echo "mean = 1.6, pi = 0.8, r=$1, nu=$2, 8 microbes per sample"
 julia simulation/run_simulation.jl -n 1 -m 1.6 -p 0.8 -r $1 -k 8 -s 2358 -u $2
