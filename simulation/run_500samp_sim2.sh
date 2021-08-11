2eb912f84ffefd95ee6ae029095c35f27ca9598f
Sam Ozminkowski <samoz@umich.edu>
2021-08-11 11:15:41 -0500

    run realistic simulations on 500 samples

---
+++ simulation/run_500samp_sim2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "realistic simulations (sim 2) - 500 samples"
+
+#additive_phylo, additive_random, interaction_phylo, interaction_random, redundant_phylo, or redundant_random
+echo "############# additive_phylo #############"
+bash simulation/run_realistic.sh 9 10 "additive_phylo" 500
+
+sleep 4s
+
+echo "############# additive_random #############"
+bash simulation/run_realistic.sh 9 10 "additive_random" 500
+
+sleep 4s
+
+echo "############# interaction_phylo #############"
+bash simulation/run_realistic.sh 9 10 "interaction_phylo" 500
+
+sleep 4s
+
+echo "############# interaction_random #############"
+bash simulation/run_realistic.sh 9 10 "interaction_random" 500
+
+sleep 4s
+
+echo "############# redundant_phylo #############"
+bash simulation/run_realistic.sh 9 10 "redundant_phylo" 500
+
+sleep 4s
+
+echo "############# redundant_random #############"
+bash simulation/run_realistic.sh 9 10 "redundant_random" 500
