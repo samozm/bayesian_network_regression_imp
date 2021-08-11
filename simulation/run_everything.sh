06f2c0213d74ab91d7a7e09477f56d585b84fe53
Sam Ozminkowski <samoz@umich.edu>
2021-08-11 11:16:33 -0500

    run realistic simulations on 500 samples

--- simulation/run_everything.sh
+++ simulation/run_everything.sh
@@ -1,7 +1,9 @@
-echo "running EVERYTHING - this will take ~ 8 hours"
+echo "running EVERYTHING - this will take ~ 30 hours"
 
 zsh simulation/run_sim1.sh
 
 zsh simulation/run_sim2.sh
 
 zsh simulation/run_500samp_sim1.sh
+
+zsh simulation/run_500samp_sim2.sh
