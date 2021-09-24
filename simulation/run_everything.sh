059a2c8cf29a3da132e7b20a797ad68a13305471
Sam Ozminkowski <samoz@umich.edu>
2021-09-24 13:05:56 -0500

    handle change in other file names

--- simulation/run_everything.sh
+++ simulation/run_everything.sh
@@ -4,6 +4,6 @@ zsh simulation/run_sim1.sh
 
 zsh simulation/run_sim2.sh
 
-zsh simulation/run_500samp_sim1.sh
+zsh simulation/run_Xsamp_sim1.sh 500
 
-zsh simulation/run_500samp_sim2.sh
+zsh simulation/run_Xsamp_sim2.sh 500
