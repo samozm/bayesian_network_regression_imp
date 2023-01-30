d9cc6993c3bc3a5c6f056c6fef5ec35fad66cac4
Sam Ozminkowski <samoz@umich.edu>
2023-01-29 18:29:53 -0600

    wagg analysis

--- additional_materials/post_analysis.R
+++ additional_materials/post_analysis.R
@@ -13,7 +13,7 @@ make.filename <- function(path,simnum,pi,mu,R,nu,n_microbes,type,samplesize,simt
   
 }
 
-check_psrf <- function(simnum,pis,mus,R,nus,n_microbes,inpath,samplesizes,simtypes=c(NULL),pref="",edge_mu=NULL)
+check_psrf <- function(simnum,pis,mus,R,nus,n_microbes,inpath,samplesizes,simtypes=c(NULL),pref="",edge_mu=NULL,cutoff=1.2)
 {
   tot <- length(pis)*length(mus)*length(R)*length(nus)*length(n_microbes)*length(samplesizes)*length(simtypes)
   max_psrf_xi <- data.frame(psrf=0,pi=0,mu=0,R=0,nu=0,n_microbes=0,samplesize=0,simtype=0)
@@ -42,11 +42,11 @@ check_psrf <- function(simnum,pis,mus,R,nus,n_microbes,inpath,samplesizes,simtyp
                     next
                   }
                   psrf <- read.csv(flnm)
-                  if(psrf$max_xi[1] > 1.2)
+                  if(psrf$max_xi[1] > cutoff)
                   {
                     max_psrf_xi <- rbind(max_psrf_xi,data.frame(psrf=psrf$max_xi[1],pi=pi,mu=mu,R=R[r.i],nu=nu,n_microbes=n_m,samplesize=samplesize,simtype=s))
                   }
-                  if(psrf$max_gamma[1] > 1.2)
+                  if(psrf$max_gamma[1] > cutoff)
                   {
                     max_psrf_gamma <- rbind(max_psrf_gamma,data.frame(psrf=psrf$max_gamma[1],pi=pi,mu=mu,R=R[r.i],nu=nu,n_microbes=n_m,samplesize=samplesize,simtype=s))
                   }
@@ -95,11 +95,11 @@ check_psrf <- function(simnum,pis,mus,R,nus,n_microbes,inpath,samplesizes,simtyp
                 flnm <- make.filename(inpath,simnum,pi,mu,R[r.i],nu,n_m,"psrf",samplesize) 
                 if(!file.exists(flnm)){next}
                 psrf <- read.csv(flnm)
-                if(psrf$max_xi[1] > 1.2)
+                if(psrf$max_xi[1] > cutoff)
                 {
                   max_psrf_xi <- rbind(max_psrf_xi,data.frame(psrf=psrf$max_xi[1],pi=pi,mu=mu,R=R[r.i],nu=nu,n_microbes=n_m,samplesize=samplesize,simtype=0))
                 }
-                if(psrf$max_gamma[1] > 1.2)
+                if(psrf$max_gamma[1] > cutoff)
                 {
                   max_psrf_gamma <- rbind(max_psrf_gamma,data.frame(psrf=psrf$max_gamma[1],pi=pi,mu=mu,R=R[r.i],nu=nu,n_microbes=n_m,samplesize=samplesize,simtype=0))
                 }
@@ -149,6 +149,10 @@ check_psrf(c(1),c("0.0","0.3","0.8"),c("0.8","1.6"),c(5,7,9),c(10),c(8,15,22),
 check_psrf(c(1),c("0.0","0.3","0.8"),c("0.8","1.6"),c(5,7,9),c(10),c(8,15,22),
            "results/simulation/wid/unrealistic/",c(100,500))
 
+#cutoff = 1.1
+check_psrf(c(1),c("0.0","0.3","0.8"),c("0.8","1.6"),c(5,7,9),c(10),c(8,15,22),
+           "results/simulation/wid/unrealistic/",c(100,500),c(NULL),"",NULL,1.1)
+
 ## 7 only
 #check_psrf(c(1),c("0.0","0.3","0.8"),c("0.8","1.6"),c(7),c(10),c(8,15,22),
 #           "results/simulation/local/unrealistic-results/",c(100,500))
@@ -165,11 +169,22 @@ check_psrf(c(1),c("0.0","0.3","0.8"),c("0.8","1.6"),c(5,7,9),c(10),c(8,15,22),
 #           c(500,1000),c("additive_phylo", "additive_random", "interaction_phylo", 
 #                         "interaction_random", "redundant_phylo", "redundant_random"),"",0.4)
 
+#check_psrf(2,c(0.3,0.8),c(0.8,1.6),c(7),c(10),c(8,22),
+#           "results/simulation/kahan/realistic/",
+#           c(500,1000),c("additive_phylo", "additive_random", "interaction_phylo", 
+#                         "interaction_random", "redundant_phylo", "redundant_random"),"",0.4)
+
 check_psrf(2,c(0.3,0.8),c(0.8,1.6),c(7),c(10),c(8,22),
-           "results/simulation/kahan/realistic/",
+           "results/simulation/wid/realistic/",
            c(500,1000),c("additive_phylo", "additive_random", "interaction_phylo", 
                          "interaction_random", "redundant_phylo", "redundant_random"),"",0.4)
 
+#cutoff = 1.1
+check_psrf(2,c(0.3,0.8),c(0.8,1.6),c(7),c(10),c(8,22),
+           "results/simulation/wid/realistic/",
+           c(500,1000),c("additive_phylo", "additive_random", "interaction_phylo", 
+                         "interaction_random", "redundant_phylo", "redundant_random"),
+           "",0.4,1.1)
 
 
 all_psrf <- function(simnum,pis,mus,R,nus,n_microbes,inpath,samplesizes,simtypes=c(NULL),pref="",edge_mu=NULL)
@@ -258,41 +273,45 @@ all_psrf <- function(simnum,pis,mus,R,nus,n_microbes,inpath,samplesizes,simtypes
 
 
 a_p <- all_psrf(2,c(0.3,0.8),c(0.8,1.6),c(7),c(10),c(8,22),
-           "results/simulation/chtc-realistic-O0-R7/",
+           "results/simulation/wid/realistic/",
            c(500,1000),c("additive_phylo", "additive_random", "interaction_phylo", 
                          "interaction_random", "redundant_phylo", "redundant_random"),"",0.4)
 
 
 psrf_xi <- a_p$xi
-psrf_xi %>% filter(psrf_xi$simtype=="additive_phylo" & psrf_xi$samplesize==500) %>% filter(psrf == max(psrf))
 psrf_xi %>% filter(psrf_xi$simtype=="additive_random" & psrf_xi$samplesize==500) %>% filter(psrf == max(psrf))
-psrf_xi %>% filter(psrf_xi$simtype=="interaction_phylo" & psrf_xi$samplesize==500) %>% filter(psrf == max(psrf))
 psrf_xi %>% filter(psrf_xi$simtype=="interaction_random" & psrf_xi$samplesize==500) %>% filter(psrf == max(psrf))
-psrf_xi %>% filter(psrf_xi$simtype=="redundant_phylo" & psrf_xi$samplesize==500) %>% filter(psrf == max(psrf))
 psrf_xi %>% filter(psrf_xi$simtype=="redundant_random" & psrf_xi$samplesize==500) %>% filter(psrf == max(psrf))
 
-psrf_xi %>% filter(psrf_xi$simtype=="additive_phylo" & psrf_xi$samplesize==1000) %>% filter(psrf == max(psrf))
+psrf_xi %>% filter(psrf_xi$simtype=="additive_phylo" & psrf_xi$samplesize==500) %>% filter(psrf == max(psrf))
+psrf_xi %>% filter(psrf_xi$simtype=="interaction_phylo" & psrf_xi$samplesize==500) %>% filter(psrf == max(psrf))
+psrf_xi %>% filter(psrf_xi$simtype=="redundant_phylo" & psrf_xi$samplesize==500) %>% filter(psrf == max(psrf))
+
 psrf_xi %>% filter(psrf_xi$simtype=="additive_random" & psrf_xi$samplesize==1000) %>% filter(psrf == max(psrf))
-psrf_xi %>% filter(psrf_xi$simtype=="interaction_phylo" & psrf_xi$samplesize==1000) %>% filter(psrf == max(psrf))
 psrf_xi %>% filter(psrf_xi$simtype=="interaction_random" & psrf_xi$samplesize==1000) %>% filter(psrf == max(psrf))
-psrf_xi %>% filter(psrf_xi$simtype=="redundant_phylo" & psrf_xi$samplesize==1000) %>% filter(psrf == max(psrf))
 psrf_xi %>% filter(psrf_xi$simtype=="redundant_random" & psrf_xi$samplesize==1000) %>% filter(psrf == max(psrf))
+
+psrf_xi %>% filter(psrf_xi$simtype=="additive_phylo" & psrf_xi$samplesize==1000) %>% filter(psrf == max(psrf))
+psrf_xi %>% filter(psrf_xi$simtype=="interaction_phylo" & psrf_xi$samplesize==1000) %>% filter(psrf == max(psrf))
+psrf_xi %>% filter(psrf_xi$simtype=="redundant_phylo" & psrf_xi$samplesize==1000) %>% filter(psrf == max(psrf))
 
 
 psrf_gamma <- a_p$gam
-psrf_gamma %>% filter(simtype=="additive_phylo" & samplesize==500) %>% filter(psrf == max(psrf))
 psrf_gamma %>% filter(simtype=="additive_random" & samplesize==500) %>% filter(psrf == max(psrf))
-psrf_gamma %>% filter(simtype=="interaction_phylo" & samplesize==500) %>% filter(psrf == max(psrf))
 psrf_gamma %>% filter(simtype=="interaction_random" & samplesize==500) %>% filter(psrf == max(psrf))
-psrf_gamma %>% filter(simtype=="redundant_phylo" & samplesize==500) %>% filter(psrf == max(psrf))
 psrf_gamma %>% filter(simtype=="redundant_random" & samplesize==500) %>% filter(psrf == max(psrf))
 
-psrf_gamma %>% filter(simtype=="additive_phylo" & samplesize==1000) %>% filter(psrf == max(psrf))
+psrf_gamma %>% filter(simtype=="additive_phylo" & samplesize==500) %>% filter(psrf == max(psrf))
+psrf_gamma %>% filter(simtype=="interaction_phylo" & samplesize==500) %>% filter(psrf == max(psrf))
+psrf_gamma %>% filter(simtype=="redundant_phylo" & samplesize==500) %>% filter(psrf == max(psrf))
+
 psrf_gamma %>% filter(simtype=="additive_random" & samplesize==1000) %>% filter(psrf == max(psrf))
-psrf_gamma %>% filter(simtype=="interaction_phylo" & samplesize==1000) %>% filter(psrf == max(psrf))
 psrf_gamma %>% filter(simtype=="interaction_random" & samplesize==1000) %>% filter(psrf == max(psrf))
-psrf_gamma %>% filter(simtype=="redundant_phylo" & samplesize==1000) %>% filter(psrf == max(psrf))
 psrf_gamma %>% filter(simtype=="redundant_random" & samplesize==1000) %>% filter(psrf == max(psrf))
+
+psrf_gamma %>% filter(simtype=="additive_phylo" & samplesize==1000) %>% filter(psrf == max(psrf))
+psrf_gamma %>% filter(simtype=="interaction_phylo" & samplesize==1000) %>% filter(psrf == max(psrf))
+psrf_gamma %>% filter(simtype=="redundant_phylo" & samplesize==1000) %>% filter(psrf == max(psrf))
 
 
 a_p_unreal <- all_psrf(1,c(0.3,0.8),c(0.8,1.6),c(7),c(10),c(8,15,22),
@@ -363,21 +382,21 @@ check_fl_exist <- function(simnum,pis,mus,R,nus,n_microbes,inpath,samplesizes,si
 #check_fl_exist(c(1),c("0.0","0.3","0.8"),c("0.8","1.6"),c(5,9),c(10),c(8,15,22),"results/simulation/chtc-unrealistic/",c(100,500))
 
 
-check_fl_exist(c(1),c("0.0","0.3","0.8"),c("0.8","1.6"),c(5,7,9),c(10),c(8,15,22),
-           "results/simulation/chtc-unrealistic-O0-R7/",c(100,500))
+#check_fl_exist(c(1),c("0.0","0.3","0.8"),c("0.8","1.6"),c(5,7,9),c(10),c(8,15,22),
+#           "results/simulation/chtc-unrealistic-O0-R7/",c(100,500))
 
 #check_fl_exist(2,c(0.3,0.8),c(0.8,1.6),c(9),c(10),c(8,22),
 #             "results/simulation/chtc-realistic/",
 #             c(500,1000),c("additive_phylo", "additive_random", "interaction_phylo", 
 #                          "interaction_random", "redundant_phylo", "redundant_random"),"",0.4)
 
-check_fl_exist(2,c(0.3,0.8),c(0.8,1.6),c(7),c(10),c(8,22),
-               "results/simulation/chtc-realistic-O0-R7/",
-               c(500,1000),c("additive_phylo", "additive_random", "interaction_phylo", 
-                             "interaction_random", "redundant_phylo", "redundant_random"),"",0.4)
+#check_fl_exist(2,c(0.3,0.8),c(0.8,1.6),c(7),c(10),c(8,22),
+#               "results/simulation/chtc-realistic-O0-R7/",
+#               c(500,1000),c("additive_phylo", "additive_random", "interaction_phylo", 
+#                             "interaction_random", "redundant_phylo", "redundant_random"),"",0.4)
 
 check_fl_exist(2,c(0.3,0.8),c(0.8,1.6),c(7),c(10),c(8,22),
-               "results/simulation/kahan/chtc-realistic-O0-R7/",
+               "results/simulation/wid/realistic/",
                c(500,1000),c("additive_phylo", "additive_random", "interaction_phylo", 
                              "interaction_random", "redundant_phylo", "redundant_random"),"",0.4)
 
@@ -446,7 +465,7 @@ check_time <- function(simnum,pis,mus,R,nus,n_microbes,inpath,samplesizes,simtyp
   return(tmz)
 }
 tms_u <- check_time(c(1),c("0.0","0.3","0.8"),c("0.8","1.6"),c(5,7,9),c(10),c(8,15,22),
-           "results/simulation/local/unrealistic-results/",c(100,500))
+           "results/simulation/wid/unrealistic/",c(100,500))
 
 seconds_to_period(mean(tms_u[tms_u$samplesize == 100 & 
                             !(tms_u$mu == 1.6 & tms_u$pi == 0.0 & tms_u$n_microbes == 22 & tms_u$R == 9) &
@@ -482,31 +501,23 @@ seconds_to_period(mean(tms_u[tms_u$samplesize == 500 &
 
 
 tms <- check_time(2,c(0.3,0.8),c(0.8,1.6),c(7),c(10),c(8,22),
-           "results/simulation/chtc-realistic-O0-R7/",
+           "results/simulation/wid/realistic/",
            c(500,1000),c("additive_phylo", "additive_random", "interaction_phylo", 
                          "interaction_random", "redundant_phylo", "redundant_random"),"",0.4)
 
 seconds_to_period(mean(tms[tms$samplesize == 500 & 
-                           !((tms$n_microbes==22 & tms$pi==0.8 & 
-                               tms$mu==1.6 & tms$samplesize==500 &
-                               tms$simtype == "redundant_random") | 
-                               (tms$n_microbes==22 & tms$pi==0.8 & 
+                           !((tms$n_microbes==22 & tms$pi==0.3 & 
                                   tms$mu==1.6 & tms$samplesize==500 &
                                   tms$simtype == "redundant_phylo") | 
-                               (tms$n_microbes==22 & tms$pi==0.3 & 
-                                tms$mu==0.8 & tms$samplesize==500 &
-                                tms$simtype == "redundant_phylo"))  & 
-                            tms$samplesize != 0, "time"]))
-seconds_to_period(mean(tms[tms$samplesize == 1000 & 
-                           !((tms$n_microbes==8 & tms$pi==0.3 & 
-                               tms$mu==0.8 & tms$samplesize==1000 &
-                               tms$simtype == "additive_random") | 
                                (tms$n_microbes==8 & tms$pi==0.3 & 
-                                  tms$mu==0.8 & tms$samplesize==1000 &
-                                  tms$simtype == "additive_phylo") |
-                               (tms$n_microbes==22 & tms$pi==0.3 & 
-                                tms$mu==0.8 & tms$samplesize==1000 &
-                                tms$simtype == "redundant_phylo")) & 
+                                tms$mu==1.6 & tms$samplesize==500 &
+                                tms$simtype == "additive_random"))  & 
+                            tms$samplesize != 0, "time"]))
+
+seconds_to_period(mean(tms[tms$samplesize == 1000 & 
+                           !((tms$n_microbes==22 & tms$pi==0.3 & 
+                                tms$mu==1.6 & tms$samplesize==1000 &
+                                tms$simtype == "redundant_random")) &
                            tms$samplesize != 0, "time"]))
 
 
